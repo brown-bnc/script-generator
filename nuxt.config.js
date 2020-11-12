4fefae1446bc441c56221b8ba7056123e5c69173
Isabel Restrepo <isabelrestre@gmail.com>
2020-11-11 21:16:03 -0500

    feat: add header

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['@brown-ccv/disco-styles'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -37,5 +37,12 @@ export default {
   modules: [],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    hotMiddleware: {
+      client: {
+        // turn off client overlay when errors are present
+        overlay: true,
+      },
+    },
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,49 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">sbatch-generator-nuxt</h1>
-      <div class="links">
+  <div>
+    <DNavbar variant="light">
+      <template v-slot:brand>
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+          class="navbar-item"
+          href="https://brown.edu"
+          aria-label="Brown's Home"
         >
-          Documentation
+          <BrownLogo class="d-nav-brand" size="l" />
         </a>
         <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
+          class="navbar-item navbar-header-end"
+          href="https://ccv.brown.edu"
+          aria-label="CCV's Home"
         >
-          GitHub
+          <CCVLogo class="d-nav-brand" size="m" />
         </a>
-      </div>
-    </div>
+      </template>
+      <template v-slot:start>
+        <li tabindex="0" class="navbar-item page-name">
+          <h2>OSCAR'S SBATCH SCRIPT GENERATOR</h2>
+        </li>
+      </template>
+    </DNavbar>
   </div>
 </template>
 
 <script>
-export default {}
+import { DNavbar, BrownLogo, CCVLogo } from '@brown-ccv/disco-vue-components'
+export default {
+  components: {
+    DNavbar,
+    BrownLogo,
+    CCVLogo,
+  },
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.page-name {
+  padding-left: 40px;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.navbar-header-end {
+  padding-right: 40px;
+  border-right: 1px solid grey;
 }
 </style>
