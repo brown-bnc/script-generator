7de0dc4ed1aa5d4259921412008d17e3114e15e7
Isabel Restrepo <isabelrestre@gmail.com>
2020-12-03 15:08:13 -0500

    feat:customizations to Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,37 +3,37 @@
     <DNavbar variant="light">
       <template v-slot:brand>
         <a
-          class="navbar-item"
+          class="navbar-item navbar-header-end"
           href="https://brown.edu"
           aria-label="Brown's Home"
         >
           <BrownLogo class="d-nav-brand" size="l" />
         </a>
-        <a
-          class="navbar-item navbar-header-end"
-          href="https://ccv.brown.edu"
-          aria-label="CCV's Home"
-        >
-          <CCVLogo class="d-nav-brand" size="m" />
-        </a>
       </template>
       <template v-slot:start>
-        <li tabindex="0" class="navbar-item page-name">
-          <h2>Behavioral Neuroimaging Core</h2>
-        </li>
+        <b-navbar-item size="large">
+          <p class="custom-size">Behavioral Neuroimaging Core</p>
+        </b-navbar-item>
+      </template>
+      <template v-slot:end>
+        <b-navbar-item>
+          <a href="https://www.brown.edu/carney/mri">MRF Facility</a>
+        </b-navbar-item>
+        <b-navbar-item>
+          <a href="https://www.brown.edu/carney">Carney</a>
+        </b-navbar-item>
       </template>
     </DNavbar>
   </div>
 </template>
 
 <script>
-import { DNavbar, BrownLogo, CCVLogo } from '@brown-ccv/disco-vue-components'
+import { DNavbar, BrownLogo } from '@brown-ccv/disco-vue-components'
 
 export default {
   components: {
     DNavbar,
     BrownLogo,
-    CCVLogo,
   },
 }
 </script>
@@ -44,7 +44,11 @@ export default {
   padding-left: 40px;
 }
 .navbar-header-end {
-  padding-right: 40px;
+  padding-right: 20px;
   border-right: 1px solid grey;
+}
+.custom-size {
+  font-size: 20px;
+  padding-left: 10px;
 }
 </style>
