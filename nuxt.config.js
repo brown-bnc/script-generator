fede0bbfeef450202ddb8c133fe71e6a5b960367
Isabel Restrepo <isabelrestre@gmail.com>
2021-02-01 22:08:43 -0500

    ci: add cd, use the disco token

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,15 @@ export default {
   ssr: false,
 
   // Target (https://go.nuxtjs.dev/config-target)
-  target: '',
+  target: 'static',
+
+  router: {
+    base: '/script-generator/'
+  }
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'sbatch-generator-nuxt',
+    title: 'bnc-script-generator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
